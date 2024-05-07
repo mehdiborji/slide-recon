0a4643a29f454d2023646e415da92e93e2f428ad
mehdiborji <borjijiji@gmail.com>
2024-05-06 23:27:33 -0400

    ruff format and some clean-up

--- gex_pipeline.py
+++ gex_pipeline.py
@@ -1,14 +1,12 @@
 import argparse
 from multiprocessing import Pool
-from matplotlib.backends.backend_pdf import PdfPages
 import bc_umi_utils
-import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--cores', type=str)
-parser.add_argument('-i', '--indir', type=str)
-parser.add_argument('-s', '--sample', type=str)
-parser.add_argument('-l', '--limit', default=False, action='store_true')
+parser.add_argument("-c", "--cores", type=str)
+parser.add_argument("-i", "--indir", type=str)
+parser.add_argument("-s", "--sample", type=str)
+parser.add_argument("-l", "--limit", default=False, action="store_true")
 
 args = parser.parse_args()
 
@@ -19,16 +17,16 @@ limit = args.limit
 
 ######################################################
 
-bc_umi_utils.split_fastq_by_lines(indir,sample,8e7)
+bc_umi_utils.split_fastq_by_lines(indir, sample, 8e7)
 
 ######################################################
 
-parts = bc_umi_utils.find_sub_fastq_parts(indir,sample)
-args = [(indir,sample,part,limit) for part in parts]
+parts = bc_umi_utils.find_sub_fastq_parts(indir, sample)
+args = [(indir, sample, part, limit) for part in parts]
 
 pool = Pool(int(cores))
 results = pool.starmap(bc_umi_utils.extract_clean_fastq, args)
 pool.close()
 pool.join()
 
-#bc_umi_utils.aggregate_dicts(indir,sample,'bcs')
+# bc_umi_utils.aggregate_dicts(indir,sample,'bcs')
