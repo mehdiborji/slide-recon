8e276f9878e38a6e13f48713f901fbe11d789d66
mehdiborji <borjijiji@gmail.com>
2024-05-06 21:04:33 -0400

    H14_1 required around 30GB RAM (vs 70GB) and 1/3 nonzero elements compared to H8_3 while having 2X features

--- SLURM_scripts/SLURM_umap.sh
+++ SLURM_scripts/SLURM_umap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -c 20
-#SBATCH --mem=80G
-#SBATCH -t 30:00:00
+#SBATCH --mem=90G
+#SBATCH -t 20:00:00
 #SBATCH -p medium
 #SBATCH -o umap_job_%A.out
 #SBATCH --account=chen_fec176
@@ -13,8 +13,13 @@ echo 'metric =' $4
 echo 'n_neighbors =' $5
 echo 'min_dist =' $6
 echo 'spread =' $7
-#echo 'subset =' $3
-#echo 'threshold =' $4
 
-python ~/reconstruction/umap_reduction.py --c 20 --i $1 --sample $2 --adata_name $3 \
---metric $4 --n_neighbors $5 --min_dist $6  --spread $7
+python ~/reconstruction/umap_reduction.py \
+        --c 20 \
+        --i $1 \
+        --sample $2 \
+        --adata_name $3 \
+        --metric $4 \
+        --n_neighbors $5 \
+        --min_dist $6  \
+        --spread $7
