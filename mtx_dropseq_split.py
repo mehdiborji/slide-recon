1ac97a1f0b5b4620a9a9c4237cf3a8989609091b
mehdiborji <borjijiji@gmail.com>
2024-05-21 13:57:44 -0400

    ruff format

--- mtx_dropseq_split.py
+++ mtx_dropseq_split.py
@@ -10,38 +10,36 @@ import scipy.sparse
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--cores', type=str)
-parser.add_argument('-i', '--indir', type=str)
-#parser.add_argument('-s', '--sample', type=str)
+parser.add_argument("-c", "--cores", type=str)
+parser.add_argument("-i", "--indir", type=str)
 
 args = parser.parse_args()
 
 cores = args.cores
 indir = args.indir
-#sample = args.sample
+
 
 def write_sparse_matrix(dge_txt):
-    
-    dge_mtx = dge_txt.replace('.txt', '_matrix.mtx.gz')
-    dge_genes = dge_txt.replace('.txt', '_features.tsv.gz')
-    dge_barcodes = dge_txt.replace('.txt', '_barcodes.tsv.gz')
-    
+    dge_mtx = dge_txt.replace(".txt", "_matrix.mtx.gz")
+    dge_genes = dge_txt.replace(".txt", "_features.tsv.gz")
+    dge_barcodes = dge_txt.replace(".txt", "_barcodes.tsv.gz")
+
     print(dge_mtx)
-    #print(dge_genes)
-    #print(dge_barcodes)
-    
-    print('get cols and rows from dge file')
+    # print(dge_genes)
+    # print(dge_barcodes)
+
+    print("get cols and rows from dge file")
     with open(dge_txt, "rt") as fh:
         rdr = csv.reader(fh, delimiter="\t")
         cols = next(rdr)[1:]
         rows = [r[0] for r in tqdm(rdr)]
-    
-    #print('write barcodes file')
-    #with gzip.open(dge_barcodes, "wt") as out:
+
+    # print('write barcodes file')
+    # with gzip.open(dge_barcodes, "wt") as out:
     #    for bc in cols:
     #        print(bc, file=out)
-    
-    print('write features (genes) file')
+
+    print("write features (genes) file")
     with gzip.open(dge_genes, "wt") as out:
         for gene in rows:
             print(gene, file=out)
@@ -60,8 +58,9 @@ def write_sparse_matrix(dge_txt):
         scipy.io.mmwrite(out, data.tocsr())
     """"""
 
+
 files = os.listdir(indir)
-files = sorted([f'{indir}/{f}' for f in files if 'header_added' in f])
+files = sorted([f"{indir}/{f}" for f in files if "header_added" in f])
 
 pool = Pool(int(cores))
 results = pool.map(write_sparse_matrix, files)
