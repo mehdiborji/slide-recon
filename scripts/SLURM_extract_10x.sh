130c19ada8d25e15656675f062b355917f4cc9a9
mehdiborji <borjijiji@gmail.com>
2024-06-14 18:03:22 -0400

    add script for running 10x spatial tagging processing

--- scripts/SLURM_extract_10x.sh
+++ scripts/SLURM_extract_10x.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -c 8
+#SBATCH -c 10
 #SBATCH --mem=10G
-#SBATCH -t 0:30:00
+#SBATCH -t 0:50:00
 #SBATCH -p priority
 #SBATCH -o slide_recon_count_job_%A.out
 #SBATCH --account=wu_cjw1
@@ -14,4 +14,5 @@ echo 'sample =' $2
 echo 'max_anchors =' $3
 echo 'max_targets =' $4
 
-python ~/reconstruction/bc_umi_pipeline.py -c 8 -i $1 -s $2 -ma $3 -mt $4 -r1 $5 -r2 $6 --limit
+python ~/reconstruction/bc_umi_pipeline_10x_spatial.py -c 20 -i $1 -s $2 -ma $3 -mt $4 -r1 $5 -r2 $6
+#--limit
